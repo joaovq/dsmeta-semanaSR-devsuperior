02747d108fe23afb5baa15e386bf2d27bbe4ab84
João Vítor <vitorsnta90@gmail.com>
2022-09-08 02:43:14 -0300

    Twilio sms

--- backend/src/main/java/com/bloomin/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/bloomin/dsmeta/controller/SaleController.java
@@ -2,23 +2,25 @@ package com.bloomin.dsmeta.controller;
 
 import com.bloomin.dsmeta.entity.Sale;
 import com.bloomin.dsmeta.service.impl.SaleServiceImpl;
+import com.bloomin.dsmeta.service.impl.SmsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
     @Autowired
     private SaleServiceImpl service;
+    @Autowired
+    private SmsServiceImpl smsService;
 
 
 //    Colocando os resultados em páginas
-//    A requisição é feita de 20 em 20
+//    A requisição é feita por cada 20
 //    Requisição no postman http://localhost:8080/sales/findSales?minDate=2021-11-21&maxDate=2022-05-04
     @GetMapping("/findSales")
     public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
@@ -27,6 +29,10 @@ public class SaleController {
         return service.getAll(minDate, maxDate, pageable);
     }
 
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
+    }
 
     @GetMapping("/get_sale/{id}")
     public ResponseEntity<Sale> getSale(@PathVariable Long id){
--- backend/src/main/java/com/bloomin/dsmeta/service/impl/SmsServiceImpl.java
+++ backend/src/main/java/com/bloomin/dsmeta/service/impl/SmsServiceImpl.java
@@ -1,4 +1,47 @@
 package com.bloomin.dsmeta.service.impl;
 
+import com.bloomin.dsmeta.entity.Sale;
+import com.bloomin.dsmeta.repository.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SmsServiceImpl {
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository repository;
+    public void sendSms(Long id) {
+
+        Sale sale = repository.findById(id).orElseThrow();
+
+        String date = sale.getDate().getMonthValue() +"/" + sale.getDate().getYear();
+
+        String msg = "O vendedor " + sale.getName() + " foi destaque em " + date +
+                " com um total de: R$ " + String.format("%.2f",sale.getAmount());
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
 }
