8f8ece2b8981e664445f72fd9d7203b6fef9393a
João Vítor <vitorsnta90@gmail.com>
2022-09-10 01:52:11 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     //Para mais de um componente, temos que colocar <> e no final </> (fragment)
     <>
+    <ToastContainer/>
       <Header/>
       {/* //Aqui chamamos o component NotificationButton que criamos para o botão de notificação */}
       <main>
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,6 @@
 //  o ..(para voltar para pastas anteriores) faz voltar para a pasta assets. 
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 // mesma pasta é um único ponto
@@ -13,7 +14,7 @@ function NotificationButton({saleId}: Props) {
 
   function handleClick(id:number){
     axios(`${BASE_URL}/sales/${id}/notification`).then(response =>{
-      console.log("Sucesso");
+      toast.info("Sms enviado com sucesso");
     })
   }
     return (
