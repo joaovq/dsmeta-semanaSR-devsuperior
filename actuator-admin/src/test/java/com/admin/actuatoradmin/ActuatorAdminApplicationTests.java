d85e4827fe93f9da13a6a32d5aef8858578ef6fb
João Vítor <vitorsnta90@gmail.com>
2022-09-12 20:19:31 -0300

    test actuator and admin-server/client

---
+++ actuator-admin/src/test/java/com/admin/actuatoradmin/ActuatorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.actuatoradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
